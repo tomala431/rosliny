92ec75e924be4f78394a3668051a286dc0e543ae
tomala431 <tomek.ostr2001@gmail.com>
2025-05-04 23:59:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "<h1>Witaj na stronie Moje Ro�liny!</h1>"
+    return "Moje Rośliny – działa!"
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
