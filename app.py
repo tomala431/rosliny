7023982d7bfc3eae3db8f0c5f02ab7b3dbbd9760
tomala431 <tomek.ostr2001@gmail.com>
2025-05-05 14:19:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-
-from flask import Flask, render_template, request, redirect, session, url_for
+from flask import Flask, render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:hHozQDcLSRTAHbQgnU7JUJeoZG9Lricw@dpg-d0bupg2dbo4c73d5fd8g-a/rosliny'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -19,37 +21,28 @@ class User(db.Model):
 
 @app.route('/')
 def index():
-    return redirect('/login')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        user = User.query.filter_by(email=email).first()
-        if user and check_password_hash(user.password, password):
-            session['user_id'] = user.id
-            return redirect('/dashboard')
-        return 'Nieprawidłowe dane logowania'
     return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         email = request.form['email']
-        password = request.form['password']
-        hashed_pw = generate_password_hash(password)
-        new_user = User(email=email, password=hashed_pw)
+        password = generate_password_hash(request.form['password'])
+        new_user = User(email=email, password=password)
         db.session.add(new_user)
         db.session.commit()
-        return redirect('/login')
+        return redirect('/')
     return render_template('register.html')
 
-@app.route('/dashboard')
-def dashboard():
-    if 'user_id' not in session:
-        return redirect('/login')
-    return render_template('dashboard.html')
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form['email']
+    password = request.form['password']
+    user = User.query.filter_by(email=email).first()
+    if user and check_password_hash(user.password, password):
+        session['user_id'] = user.id
+        return 'Zalogowano pomyślnie'
+    return 'Błędny login lub hasło'
 
 if __name__ == '__main__':
     with app.app_context():
