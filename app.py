7eafd38776e90ec93ea27d59c1313cc87820aeab
tomala431 <tomek.ostr2001@gmail.com>
2025-05-05 11:52:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,22 @@
+
 from flask import Flask, render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
 
 app = Flask(__name__)
-app.secret_key = os.urandom(24)  # bezpieczny klucz sesji
+app.secret_key = os.urandom(24)
 
-# KONFIGURACJA DB
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:hHozQDoLSRTAHbQgnU7JUJeoZG9Lricw@dpg-d0bupg2dbo4c73d5fd8g-a/rosliny'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-# MODEL UŻYTKOWNIKA
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
 
-# ROUTES
 @app.route('/')
 def index():
     return render_template('login.html')
@@ -28,10 +26,8 @@ def register():
     if request.method == 'POST':
         email = request.form['email']
         password = generate_password_hash(request.form['password'])
-
         if User.query.filter_by(email=email).first():
             return 'Użytkownik już istnieje'
-        
         new_user = User(email=email, password=password)
         db.session.add(new_user)
         db.session.commit()
@@ -42,7 +38,6 @@ def register():
 def login():
     email = request.form['email']
     password = request.form['password']
-
     user = User.query.filter_by(email=email).first()
     if user and check_password_hash(user.password, password):
         session['user_id'] = user.id
