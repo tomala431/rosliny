b3d71472f558c63e1f16201ffc26294e2a29693a
tomala431 <tomek.ostr2001@gmail.com>
2025-05-06 19:01:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, g
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
