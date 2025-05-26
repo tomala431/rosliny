c4794c5935a6005539cbc31bece4ee4887038edf
tomala431 <tomek.ostr2001@gmail.com>
2025-05-26 21:44:15 +0200

    Update flowers.js

--- static/flowers.js
+++ static/flowers.js
@@ -1,10 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const flowerBox = document.getElementById("flowerBox");
 
-    // Liczba roślin – renderowana dynamicznie
-    const flowerCount = {{ plants|length }};
-
-    for (let i = 0; i < flowerCount; i++) {
+    for (let i = 0; i < plantCount; i++) {
         const flower = document.createElement("div");
         flower.classList.add("flower");
         flowerBox.appendChild(flower);
