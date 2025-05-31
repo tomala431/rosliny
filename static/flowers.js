be32ac9ef15c983891b0263d25085a608bd2aa26
tomala431 <tomek.ostr2001@gmail.com>
2025-05-31 16:36:38 +0200

    Update flowers.js

--- static/flowers.js
+++ static/flowers.js
@@ -1,9 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
     const flowerBox = document.getElementById("flowerBox");
 
+    // Tablica dostępnych obrazków kwiatków
+    const flowerImages = [
+        '/static/icons/flower1.gif',
+        '/static/icons/flower2.gif',
+        '/static/icons/flower3.gif'
+    ];
+
     for (let i = 0; i < plantCount; i++) {
         const flower = document.createElement("div");
         flower.classList.add("flower");
+
+        // Losowy wybór obrazka
+        const randomIndex = Math.floor(Math.random() * flowerImages.length);
+        const selectedImage = flowerImages[randomIndex];
+
+        // Ustawiamy tło na losowy obrazek
+        flower.style.backgroundImage = `url('${selectedImage}')`;
+
         flowerBox.appendChild(flower);
     }
 });
